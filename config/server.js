c2b55c308dabbc64f6bf0ab20aaa788398d2eff1
Omkar Hase <omkarhase.dev@gmail.com>
2025-12-29 18:49:34 +0530

    Fix 502 error

--- config/server.js
+++ config/server.js
@@ -1,8 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: "0.0.0.0",
+  port: env.int("PORT", 1337), // <- Use Render's port
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    apiToken: {
+      salt: env("API_TOKEN_SALT"),
+    },
   },
 });
-//hrererrrere
